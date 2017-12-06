a5bc5ebd077d51f95b2b078032d41ab10eb3cc96
Gebbyanna <gebbypahriana1302@gmail.com>
2017-12-06 18:02:27 +0700

    Film Kartun gebby

---
+++ U_kartun1.pas
@@ -0,0 +1,51 @@
+unit U_kartun1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls;
+
+type
+  TForm12 = class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    Edit6: TEdit;
+    Edit7: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+    Label8: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form12: TForm12;
+
+implementation
+
+uses Unit2, Unit1;
+
+{$R *.dfm}
+
+procedure TForm12.Button1Click(Sender: TObject);
+begin
+form2.showmodal;
+end;
+
+end.
